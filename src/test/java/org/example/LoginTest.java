cfaf9a1956213f9766907ec94db89695549bd65f
aaditya mankar <aadityamankar18@gmail.com>
2026-06-06 21:10:03 +0530

    Updated the LoginTest.java file

--- src/test/java/org/example/LoginTest.java
+++ src/test/java/org/example/LoginTest.java
@@ -1,35 +1,55 @@
 package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
-
 public class LoginTest {
-    WebDriver driver;   // 👈 class-level driver
-    @Test(priority = 1)
-    public void launchDriver() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\aadit\\OneDrive\\Documents\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-        driver = new ChromeDriver();   // 👈 initialize here
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait((Duration.ofSeconds(10)));
+        System.out.println("Test 1: started");
+    }
+
+    @Test(priority = 1)
+    public void openFirstUrl() {
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
-        System.out.println("Test 1 title is: " + driver.getTitle());
-
+        System.out.println("Test 2: Title is = " + driver.getTitle());
     }
 
-    @Test (priority = 2)
-    public void navigate() throws InterruptedException {
+    @Test(priority = 2)
+    public void openSecondUrl() throws IOException {
         driver.navigate().to("https://practicetestautomation.com/practice-test-login/");
-        System.out.println("Test 2 title is: " + driver.getTitle());
-
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("submit")));
+        File src =((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(src, new File("C:\\Users\\aadit\\IdeaProjects\\dummyTest_03\\screenshot\\login_page.png"));
+        System.out.println("Test 3: Title is = " + driver.getTitle());
     }
 
-    @Test (priority = 3)
-    public void quitDriver() throws InterruptedException {
-
-        Thread.sleep(2000);
-        driver.quit();
-        System.out.println("Test 2 title is: Driver Closed");
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            System.out.println("Test 4: Driver Closed");
+        }
     }
 }
